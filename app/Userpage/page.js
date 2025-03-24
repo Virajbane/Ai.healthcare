94510762310cc9c38b61ea0e5ff4492f2a710642
Viraj Bane <125433539+Virajbane@users.noreply.github.com>
2025-03-24 20:00:24 +0530

    chat bot added

--- app/Userpage/page.js
+++ app/Userpage/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '@/components/Header'
+import ChatbotInterface from '@/components/chatbot'
 
 function Userpage() {
   return (
     <div>
       <Header/>
+      <ChatbotInterface/>
     </div>
   )
 }
